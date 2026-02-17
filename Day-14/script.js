37e3af987cf2bd3285107bfac97ec6a308396d09
Anveshna <anveshna2025@gmail.com>
2026-02-17 15:35:26 +0530

    new commit

--- Day-14/script.js
+++ Day-14/script.js
@@ -1,3 +1,36 @@
-// syntax for Promise
-new Promise(()=>{});
-console.log(myPromise);
+// // syntax for Promise
+// //promises are smart organisation of call back functions
+// //importan for interviews
+// let myPromise=new Promise((res,rej)=>{
+//     let data = "this is data"
+//     if(data){
+//         res(data);
+//     } else{
+//         rej("error")
+//     }
+    
+// });
+// myPromise.then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+// console.log(myPromise);
+
+
+
+
+//fetch function - usedto fetch data from an api
+//you handle promise by using .then
+// let resp=fetch("https://jsonplaceholder.typicode.com/todos/1")
+// resp.then((robj)=>{
+//     console.log(robj);
+//     console.log(robj.json());
+// })
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+.then((robj)=>{
+    robj.json().then((data)=>{
+        console.log(data);
+    });
+   
+})
