67144b5c370c8d1eb603ae5d26f100ec05ef844f
Anveshna <anveshna2025@gmail.com>
2026-03-02 15:31:53 +0530

    new commit

--- Day-17/script.js
+++ Day-17/script.js
@@ -11,12 +11,29 @@
 // .then(()=>console.log("promise resolved"))
 // .catch((err)=>console.error(err,"promise rejected"));
 
-function pro(){
+// function pro(){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//             res();
+//         },1000)
+//     })
+//     .then(()=>console.log("resolved"))
+// }
+// console.log(pro());
+
+
+
+function pro(num){
     return new Promise((res,rej)=>{
         setTimeout(()=>{
+            console.log(num);
             res();
         },1000)
     })
-    .then(()=>console.log("resolved"))
 }
-console.log(pro());
+pro(1)
+.then(()=>pro(2))
+.then(()=>pro(3))
+.then(()=>pro(4))
+.then(()=>pro(5))
+.catch(()=>console.log("error"))
