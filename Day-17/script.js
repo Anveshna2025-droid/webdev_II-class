60b9cdf8bcaa44611a16f12eaccdb60317a03cc8
Anveshna <anveshna2025@gmail.com>
2026-03-16 14:38:32 +0530

    new commit

--- Day-17/script.js
+++ Day-17/script.js
@@ -23,6 +23,29 @@
 
 
 
+//promise chaining: when we use multiple .then method to achieve the task or result.
+
+//in callback hell we did the same task but the code is hard to debug and structure is not good
+
+// function pro(num){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//             console.log(num);
+//             res();
+//         },1000)
+//     })
+// }
+// pro(1)
+// .then(()=>pro(2))
+// .then(()=>pro(3))
+// .then(()=>pro(4))
+// .then(()=>pro(5))
+// .catch(()=>console.log("error"))
+
+
+//disadvantages : to perform certain task we need to call a callback function multiple times . more functions for smaller task means more complexity and time for executuion and function call will be more .
+
+
 function pro(num){
     return new Promise((res,rej)=>{
         setTimeout(()=>{
@@ -31,9 +54,10 @@ function pro(num){
         },1000)
     })
 }
-pro(1)
-.then(()=>pro(2))
-.then(()=>pro(3))
-.then(()=>pro(4))
-.then(()=>pro(5))
-.catch(()=>console.log("error"))
+ let prores=Promise.resolve();
+ 
+ for(let i=1;i<=5;i++){
+     prores=prores.then(()=>pro(i))
+ }
+
+ 
